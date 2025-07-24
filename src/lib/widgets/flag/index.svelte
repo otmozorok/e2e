57c5fbad3a604f59c55f3a2f42f7044a2d8c44f7
Boris Hasikov <laapl@ya.ru>
2025-07-25 00:45:32 +0300

    Сделал другой бинд

--- src/lib/widgets/flag/index.svelte
+++ src/lib/widgets/flag/index.svelte
@@ -6,33 +6,15 @@
 	let { scene, folder, dispatch } = useStoreon('scene', 'folder');
 
 	let show = $state(false);
-	let clickCount = $state(0);
-	let timer = $state<NodeJS.Timeout | null>(null);
 	let open = $derived(!!$folder || !!$scene);
 
 	function openFolder() {
 		dispatch(SceneEvent.ChangeScene, 'folder');
 	}
 
-	function secretAction() {
-		clickCount++;
-
-		if (clickCount === 1) {
-			timer = setTimeout(() => {
-				if (clickCount === 1) {
-					openFolder();
-				}
-				clickCount = 0;
-			}, 1000 * 1.5);
-		}
-
-		if (clickCount === 10) {
-			clearTimeout(timer as unknown as number);
-
-			show = !show;
-
-			clickCount = 0;
-		}
+	function secretAction(e: Event) {
+		e.preventDefault();
+		show = !show;
 	}
 </script>
 
@@ -46,21 +28,15 @@
 	<div class="h-full w-full translate-x-1/2 scale-250 rotate-x-60 animate-wave bg-[#0039A6]"></div>
 	<div class="h-full w-full translate-x-1/2 scale-250 rotate-x-60 animate-wave bg-[#D52B1E]"></div>
 
-	{#if show}
-		<div
-			class="bg-[url(./bg.png)] bg-no-repeat bg-cover bg-center absolute inset-0 opacity-50"
-			transition:fade
-		></div>
-	{/if}
-
 	{#if !open}
 		<img
 			src="./gerb.svg"
 			width="300px"
-			class="absolute z-20 cursor-pointer transition-all duration-300 drop-shadow-2xl active:scale-110"
+			class="absolute z-30 cursor-pointer transition-all duration-300 drop-shadow-2xl active:scale-105"
 			alt=""
 			transition:scale={{ duration: 100 }}
-			onclick={secretAction}
+			onclick={openFolder}
+			oncontextmenu={secretAction}
 		/>
 	{/if}
 
@@ -70,4 +46,11 @@
 			open ? 'bg-black/90 backdrop-blur-3xl' : 'bg-black/70 backdrop-blur-xs'
 		]}
 	></div>
+
+	{#if show}
+		<div
+			class="bg-[url(./bg.png)] z-20 bg-no-repeat bg-cover bg-center absolute inset-0 opacity-50"
+			transition:fade
+		></div>
+	{/if}
 </div>
